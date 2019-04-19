04a1c10c39c017ff2f79eab5986d202ea09ce84b
Miguel Ángel Martínez Amézquita <miguelmartinezaa@hotmail.com>
2019-04-19 17:03:44 -0500

    Subir 2019-04-19

---
+++ NorthWind.DAL/NorthWind.DAL/NorthwindContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWind.Entities;
+
+namespace NorthWind.DAL
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;database=NorthWind;Trusted_Connection=True;");
+        }
+    }
+
+}
