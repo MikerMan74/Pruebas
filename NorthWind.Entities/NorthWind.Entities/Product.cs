785f1984c0a1d16aebdbd972e99cafd303f442a8
Miguel Ángel Martínez Amézquita <miguelmartinezaa@hotmail.com>
2019-04-18 22:20:38 -0500

    Inicio1

---
+++ NorthWind.Entities/NorthWind.Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWind.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
